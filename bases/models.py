2f4b6cedd55f69218bd56e052cac5c2daafcfdf9
jpurrac <jpurrac@gmail.com>
2020-02-22 18:50:35 -0300

    22-02-2020 18:50hrs

--- bases/models.py
+++ bases/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django_userforeignkey.models.fields import UserForeignKey
 
 class ClaseModelo(models.Model):
     #variables de la clase
@@ -13,3 +14,14 @@ class ClaseModelo(models.Model):
     class Meta:
         abstract = True
 
+class ClaseModelo2(models.Model):
+    estado = models.BooleanField(default=True) #estado
+    fc = models.DateTimeField(auto_now_add=True) #fecha creacion, se agregará automaticamente
+    fm = models.DateTimeField(auto_now=True) # fehcamodificacion
+   # uc = models.ForeignKey(User, on_delete=models.CASCADE)
+   # um = models.IntegerField(blank=True, null = True)
+    uc = UserForeignKey(auto_user_add=True, related_name='+')
+    um = UserForeignKey(auto_user=True, related_name='+')
+
+    class Meta:
+        abstract = True
