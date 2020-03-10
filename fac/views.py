c8a5d9ea737a8b5529a9a0a190bdbda1c7a57ae9
jpurrac <jpurrac@gmail.com>
2020-03-10 18:23:35 -0300

    10-03-2020 18:23Hrs

--- fac/views.py
+++ fac/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth.decorators import login_required, permission_required
 from django.http import HttpResponse
 
 from bases.views import SinPrivilegios
-from .models import Cliente
+from .models import Cliente, FacturaEnc, FacturaDet
 from .forms import ClienteForm
 
 
@@ -63,3 +63,16 @@ def clienteInactivar(request, id):
             return HttpResponse("OK")
         return HttpResponse("FAIL")
     return HttpResponse("FAIL")
+
+class FacturaView(SinPrivilegios, generic.ListView):
+    model = FacturaEnc
+    template_name = "fac/factura_list.html"
+    context_object_name = "obj"
+    permission_required = "fac.view_facturaenc"
+
+@login_required(login_url="/login/")
+@permission_required("fac.change_facturaenc", login_url="bases:sin_privilegios")
+def facturas(request, id=None):
+    template_name='fac/facturas.html'
+    contexto = {}
+    return render(request, template_name, contexto)
