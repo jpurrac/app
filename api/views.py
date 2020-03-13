51681f6732f90492273b7a46eaf9e644c3e25aef
jpurrac <jpurrac@gmail.com>
2020-03-13 18:08:10 -0300

    13-03-2020

--- api/views.py
+++ api/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+
+from .serializers import ProductoSerializer
+from inv.models import Producto
+
+class ProductoList(APIView):
+    def get(self, request):
+        prod = Producto.objects.all()
+        data = ProductoSerializer(prod, many=True).data #se serializa prod
+        return Response(data)
+
+class ProductoDetalle(APIView):
+    def get(self, request, codigo):
+        prod = get_object_or_404(Producto, codigo=codigo)
+        data = ProductoSerializer(data).data #se serializa prod.
+        return Response(data)
