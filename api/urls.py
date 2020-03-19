36f1ce9fdd91bb94587073af55945c3a4ca43363
jpurrac <jpurrac@gmail.com>
2020-03-19 02:01:14 -0300

    19-03-2020 02:00
    NoReverseMatch at /fac/facturas/new

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from .views import ProductoList, ProductoDetalle
 
 
-    urlspatterns = [
-        path('v1/productos/', ProductoList.as_view, name="producto_list"),
-        path('v1/productos/<str:codigo>', ProductoList.as_view(), name='producto_detalle'),
-    ]
+urlpatterns = [
+    path('v1/productos/', ProductoList.as_view(), name="producto_list"),
+    path('v1/productos/<str:codigo>', ProductoDetalle.as_view(), name='producto_detalle'),
+]
